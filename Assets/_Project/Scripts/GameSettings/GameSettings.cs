c903c36b8b67a73ad1d9923bfb4f4bb47239259d
DinhHoHo <hpv25072004@gmail.com>
2026-08-04 20:05:28 +0700

    Add change environment setting

--- Assets/_Project/Scripts/GameSettings/GameSettings.cs
+++ Assets/_Project/Scripts/GameSettings/GameSettings.cs
@@ -1,9 +1,16 @@
 using UnityEngine;
 
+public enum DayNightSetting
+{
+    Day = 0,
+    Night = 1
+}
+
 public static class GameSettings
 {
     private const string ShowGasLevelKey = "ShowGasLevel";
     private const string ShowTimeKey = "ShowTime";
+    private const string DayNightKey = "DayNight";
 
     public static bool ShowGasLevel
     {
@@ -16,6 +23,17 @@ public static class GameSettings
         get => PlayerPrefs.GetInt(ShowTimeKey, 1) == 1;
         set => PlayerPrefs.SetInt(ShowTimeKey, value ? 1 : 0);
     }
+
+    public static DayNightSetting DayNight
+    {
+        get => PlayerPrefs.GetInt(DayNightKey, (int)DayNightSetting.Day) ==
+               (int)DayNightSetting.Night
+            ? DayNightSetting.Night
+            : DayNightSetting.Day;
+        set => PlayerPrefs.SetInt(DayNightKey, (int)value);
+    }
+
+    public static bool IsNight => DayNight == DayNightSetting.Night;
 
     public static void Save()
     {
